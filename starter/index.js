f5a1cd91b9fccf65927e278197f5214c7524a9b9
Amey Joshi <ameyjoshi2001rj@gmail.com>
2023-02-18 23:14:50 +0530

    basic node demo

---
+++ starter/index.js
@@ -0,0 +1,9 @@
+//Documentation of node.js (links) :
+// https://nodejs.org/dist/latest-v18.x/docs/api/repl.html  
+// https://nodejs.org/dist/latest-v18.x/docs/api/fs.html#fsreadfilesyncpath-options
+
+// File system module.
+const fs = require('fs');
+
+const hello = "Hello World";
+console.log(hello);

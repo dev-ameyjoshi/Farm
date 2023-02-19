afffa494c9da85b0844d5b2088fdfcd37576c01d
Amey Joshi <ameyjoshi2001rj@gmail.com>
2023-02-19 09:34:47 +0530

    Reading and writing files in node.js

--- starter/index.js
+++ starter/index.js
@@ -5,5 +5,12 @@
 // File system module.
 const fs = require('fs');
 
-const hello = "Hello World";
-console.log(hello);
+// const hello = "Hello World";
+// console.log(hello);
+
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+//Writing to files in node js
+const textOut = `This is the information we have about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File written!');

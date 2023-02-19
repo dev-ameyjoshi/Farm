ae0afe3577171ed6fe628013df2126f9c463b080
Amey Joshi <ameyjoshi2001rj@gmail.com>
2023-02-19 22:26:30 +0530

    async and sync,callback functions

--- starter/index.js
+++ starter/index.js
@@ -5,12 +5,45 @@
 // File system module.
 const fs = require('fs');
 
+
+//Synchronus way : 
+
+
 // const hello = "Hello World";
 // console.log(hello);
 
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
+
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(textIn);
+
+
 //Writing to files in node js
-const textOut = `This is the information we have about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File written!');
+
+// const textOut = `This is the information we have about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('File written!');
+
+
+// Non-Blocking asynchronus way:
+fs.readFile('./txt/start.txt','utf-8', (err,data1) => {
+        if(err) return console.log("Error Found!");
+        fs.readFile(`./txt/${data1}.txt`,'utf-8', (err,data2) => {
+                console.log(data2);
+                fs.readFile(`./txt/append.txt`,'utf-8', (err,data3) => {
+                        console.log(data3);
+                        fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err => {
+                           console.log("Your file has been written :)")
+                        })
+                });
+        });
+        
+});
+
+console.log("reading file.....");
+
+
+//regular function and async function has difference in :
+// syntax
+// and the async function does not get its own disk function and uses the disk keyword from the parent function.
+// and is called the lexical disk keyword.
+// where as the regular function does always gets its own disk keyword function.
